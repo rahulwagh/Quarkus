dff6c6959029c5c0d1b8bae7f0e23d9c4cb2afa6
rahul <rahul.wagh17@gmail.com>
2020-03-25 20:38:48 +0200

    Initial Commit

---
+++ src/native-test/java/com/jhooq/NativeHelloJhooqIT.java
@@ -0,0 +1,9 @@
+package com.jhooq;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloJhooqIT extends HelloJhooqTest {
+
+    // Execute the same tests but in native mode.
+}
